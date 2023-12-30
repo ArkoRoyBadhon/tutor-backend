9ab041a58fb19fb05d60af58878de94ba9f58e4c
Arko <arkoroyb@gmail.com>
2023-12-30 15:11:23 +0600

    lint-staged, husky setup

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
-import express, { Application } from "express";
+import express, { Application } from 'express'
 import cors from 'cors'
-const app: Application = express();
-const port = 5000;
+const app: Application = express()
+const port = 5000
 
 app.use(cors())
 
 //parser
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-  res.send("working successfully");
-});
-
+app.get('/', (req, res) => {
+  res.send('working successfully')
+})
 
 export default app
